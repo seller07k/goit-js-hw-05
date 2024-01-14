b3a823276d27c789d0ff2a0b81c32b0c898ba046
seller07k <138016173+seller07k@users.noreply.github.com>
2024-01-14 23:21:14 +0200

    1

--- js/task-4.js
+++ js/task-4.js
@@ -4,7 +4,7 @@ const getTotalBalanceByGender = (users, gender) => {
     .reduce((totalBalance, user) => totalBalance + user.balance, 0);
 };
 
-const allUsers = [
+const allUserss = [
   {
     name: "Moore Hensley",
     gender: "male",
@@ -42,6 +42,6 @@ const allUsers = [
   },
 ];
 
-console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
+console.log(getTotalBalanceByGender(allUserss, "male")); // 12053
 
-console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
+console.log(getTotalBalanceByGender(allUserss, "female")); // 8863
